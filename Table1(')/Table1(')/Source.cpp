bf20189559376f81621cbb2075b94ca0b64e2911
st117208 <st117208.student.spbu.ru>
2025-04-19 13:05:49 +0300

    code upload

--- Table1(')/Table1(')/Source.cpp
+++ Table1(')/Table1(')/Source.cpp
@@ -6,14 +6,25 @@
 
 void in(float a[], int n)
 {
-    std::ifstream inputFile("Input.csv");
-    float col1 = 0;
-    float col2 = 0;
+    std::ifstream in("Input.csv");
+    float col = 0;
     for (int i = 0; i < n; i++)
     {
-        inputFile >> col1 >> col2;
-        a[i] = col2;
+        in >> col;
+        a[i] = col;
     }
+}
+
+float roundx(float m, int n)
+{
+    if (m == 0)
+    {
+        return 0;
+    }
+    int exp = static_cast<int>(std::floor(std::log10(std::abs(m))));
+    float val = std::pow(10, n - exp);
+    float tmp = round(m * val) / val;
+    return tmp;
 }
 
 void amperage(float a[], int n, float r)
@@ -26,17 +37,13 @@ void amperage(float a[], int n, float r)
 
 void out(float a[], int n, float r)
 {
-    std::ofstream outputFile("Output.csv");
+    std::ofstream out("Output.csv");
     amperage(a, n, r);
-    float val = 0;
-    float tmp = 0;
 
     for (int i = 0; i < n; i++)
     {
-        int exp = static_cast<int>(std::floor(std::log10(std::abs(a[i]))));
-        val = std::pow(10, 1 - exp);
-        tmp = round(a[i] * val) / val;
-        outputFile << tmp << " " << std::setprecision(2) << log(tmp) << std::endl;
+        float col = roundx(a[i], 1);
+        out << col << " " << std::setprecision(2) << log(col) << std::endl;
     }
 
 }
